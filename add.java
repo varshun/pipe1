9bc7307c1c2d951807ab2e76c929330943b04ced
Varshun <varshun.tiku@nihilent.com>
2020-02-21 15:43:11 +0530

    add.java Added

---
+++ add.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class add
+{ 
+   public static void main(String[] args) 
+   {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+
+}
